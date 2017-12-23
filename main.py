2b8d1a8efb23b33c2329fca571e85cbc619dfbf8
DrMad92 <Tipulu92>
2017-12-23 15:51:46 +0200

    Added command line arguments support

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import urllib.request
 import datetime
 import argparse
 
+defaultName = "data/btc-marketprice-" + str(datetime.date.today()) + ".csv"
 
-def main():
-    print("Hi from main")
-    filename = "data/btc-marketprice-" + str(datetime.date.today()) + ".csv"
-    urllib.request.urlretrieve("https://api.blockchain.info/charts/market-price?format=csv", filename)  # Python 3
+
+def main(filename=defaultName):
+    print("Hi from main: " + filename)
+    #urllib.request.urlretrieve("https://api.blockchain.info/charts/market-price?format=csv", filename)  # Python 3
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Shows info about BTC market based on csv file')
+    parser.add_argument('-f', dest='filename', help='path to a file')
+    args = parser.parse_args()
+    if args.filename is not None:
+        main(args.filename)
+    else:
+        main()
