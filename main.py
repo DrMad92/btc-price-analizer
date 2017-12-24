e7e1157e89dfccab0b24815edfce17caca98720a
DrMad92 <Tipulu92>
2017-12-24 11:22:51 +0200

    Added greatest_increse, greatest_decrease and highest_price functions

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import urllib.request
 import datetime
 import argparse
 import csv
+from collections import namedtuple
 
 defaultName = "data/btc-marketprice-" + str(datetime.date.today()) + ".csv"
 days = []
 values = []
+Point = namedtuple('Point', ['day', 'value'])
 
 
 def main(filename):
@@ -18,7 +20,7 @@ def main(filename):
                 reader = csv.reader(csvfile, delimiter=',')
                 for row in reader:
                     days.append(row[0])
-                    values.append(row[1])
+                    values.append(float(row[1]))
         except Exception as e:
             print(e)
             print('Exiting')
@@ -34,6 +36,65 @@ def main(filename):
             print(e)
             print('Exiting')
             exit()
+
+    raw_data = list(zip(days, values))
+    data = [Point(day=day, value=value) for day, value in raw_data]
+    greatest_increase(data)
+    greatest_decrease(data)
+    highest_price(data)
+
+
+def greatest_increase(data):
+    biggest_change = 0
+    day_of_biggest_change = None
+
+    for i in range(len(data) - 1):
+        previous = data[i].value
+        current = data[i + 1].value
+        if current > previous:
+            change = current * 100 / previous
+            change = change - 100
+            if change >= biggest_change:
+                biggest_change = change
+                day_of_biggest_change = data[i + 1].day
+        else:
+            continue
+
+    print('\nBiggest percent increase: ', biggest_change)
+    print('Day: ', day_of_biggest_change)
+
+
+def greatest_decrease(data):
+    biggest_change = 0
+    day_of_biggest_change = None
+
+    for i in range(len(data) - 1):
+        previous = data[i].value
+        current = data[i + 1].value
+        if current < previous:
+            change = current * 100 / previous
+            change = 100 - change
+            if change >= biggest_change:
+                biggest_change = change
+                day_of_biggest_change = data[i + 1].day
+        else:
+            continue
+
+    print('\nBiggest percent decrease: ', biggest_change)
+    print('Day: ', day_of_biggest_change)
+
+
+def highest_price(data):
+    max_price = 0
+    day_of_max_price = None
+
+    for data_point in data:
+        if max_price <= data_point.value:
+            max_price = data_point.value
+            day_of_max_price = data_point.day
+
+    print('\nHighest price: ', max_price)
+    print('Day: ', day_of_max_price)
 
 
 if __name__ == "__main__":
